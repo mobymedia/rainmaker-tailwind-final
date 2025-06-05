317ea0d41abba9e47a210e85f0070efded73a7bb
Moby Media <166937848+mobymedia@users.noreply.github.com>
2025-06-05 16:08:09 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+import "../styles/globals.css";
+import { ReactNode } from "react";
+
+export const metadata = {
+  title: "Rainmaker",
+  description: "A beautiful multichain multisender dApp",
 };
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
